d3ad9d72f16fad502db5794a4f558a78d1a7ea47
Ubuntu <ubuntu@CloudCompute>
2020-12-09 19:08:02 -0500

    changed sampling pipeline in preprocessing script

--- preprocess_data.py
+++ preprocess_data.py
@@ -3,6 +3,9 @@ import json
 import pickle
 import collections
 import random
+from math import ceil
+import heapq
+from tqdm import tqdm
 
 data_dir = sys.argv[1]
 
@@ -17,109 +20,149 @@ for f in train_data_files: print(f)
 print("")
 
 dev_dir = os.path.join(data_dir,"dev")
-dev_data_files = os.listdir(dev_dir)
+dev_data_files = [x for x in os.listdir(dev_dir) if x.endswith("jsonl")]
 print("There are ", len(dev_data_files), "validation JSON files")
 print("Listed below")
 for f in dev_data_files: print(f)
 print("")
 
-def extract_qa(line_dict):
-	question_text = line_dict['question_text'] + '?'
+def find_closest_len_candidate_ids(cands, length, k):
+	return heapq.nsmallest(k, cands, key=lambda x: abs(x[0]-length))
 
-	for la in line_dict['annotations']:
-		long_answer = la['long_answer']
+def find_smallest_len_candidate_ids(cands, k):
+	return heapq.nsmallest(k, cands)
+
+def find_largest_len_candidate_ids(cands, k):
+	return heapq.nlargest(k, cands)
+ 
+def extract_qa(line_dict, training=True):
+	question_text = line_dict['question_text'] + '?'
+	if training:
+		long_answer = line_dict['annotations'][0]['long_answer']
 		la_start_byte = long_answer['start_byte']
 		la_end_byte = long_answer['end_byte']
+		if la_start_byte < 0 and la_end_byte < 0: 
+			return
+
 		long_answer_content = ""
 		for l in line_dict['document_tokens']:
 			if l['start_byte'] >= la_start_byte and l['end_byte'] <= la_end_byte and l['html_token'] is False:
 				 long_answer_content += l['token'] + " "
-	
-	lac_starts = collections.defaultdict(list)
-	lac_ends = collections.defaultdict(list)
-	i = 0
-	for lac in random.sample(line_dict['long_answer_candidates'], min(5, len(line_dict['long_answer_candidates']))):
-		if lac['start_byte'] == la_start_byte and lac['end_byte'] == la_end_byte:
-			continue
-		start_byte = lac['start_byte']
-		end_byte = lac['end_byte']
-		lac_starts[start_byte].append(i)
-		lac_ends[end_byte].append(i)
-		i += 1
+		long_answer_len = la_end_byte - la_start_byte + 1
 
-	lac_contents = ["" for x in range(len(lac_starts))]
-	curr_spans = set()
-	for l in line_dict['document_tokens']:
+		candidates_start_end_len = [( lac['end_byte']-lac['start_byte']+1, lac['start_byte'], lac['end_byte'] ) for lac in line_dict['long_answer_candidates']]
+		heapq.heapify(candidates_start_end_len)
 
-		if l['start_byte'] in lac_starts:
-			for x in lac_starts[l['start_byte']]:
-				curr_spans.add(x)
+		num_of_candidates = ceil(0.10*len(line_dict['long_answer_candidates']))
+		closest_cands = find_closest_len_candidate_ids(candidates_start_end_len, long_answer_len, ceil(0.5 * num_of_candidates))
+		smallest_cands = find_smallest_len_candidate_ids(candidates_start_end_len, ceil(0.25 * num_of_candidates))
+		largest_cands = find_largest_len_candidate_ids(candidates_start_end_len, ceil(0.25 * num_of_candidates))
+		all_cands = closest_cands + smallest_cands + largest_cands
+		all_cands = list(set(all_cands))
 
-		if l['end_byte'] in lac_ends:
-			for x in lac_ends[l['end_byte']]:
-				curr_spans.remove(x)
+		lac_starts = collections.defaultdict(list)
+		lac_ends = collections.defaultdict(list)
+		i = 0
+		for lac in all_cands:
+			start_byte = lac[1]
+			end_byte = lac[2]
+			if start_byte == la_start_byte and end_byte == la_end_byte:
+				continue
+			lac_starts[start_byte].append(i)
+			lac_ends[end_byte].append(i)
+			i += 1
+		
+		num_of_candiates = len(all_cands)
+		lac_contents = ["" for x in range(i)]
+		curr_spans = set()
+		for l in line_dict['document_tokens']:
 
-		for span in curr_spans:
-			lac_contents[span] += l['token'] + " "
-	
-	return [question_text, long_answer_content, lac_contents]
+			if l['start_byte'] in lac_starts:
+				for x in lac_starts[l['start_byte']]:
+					curr_spans.add(x)
 
-def preprocess_data(full_data):
+			if l['end_byte'] in lac_ends:
+				for x in lac_ends[l['end_byte']]:
+					curr_spans.remove(x)
+
+			for span in curr_spans:
+				lac_contents[span] += l['token'] + " "
+		return [question_text, long_answer_content, lac_contents]
+
+	else:
+		long_answers = []
+		la_start_bytes_set = set()
+		la_end_bytes_set = set()
+		for la in line_dict['annotations']:
+			long_answer = la['long_answer']
+			la_start_byte = long_answer['start_byte']
+			la_end_byte = long_answer['end_byte']
+			if la_start_byte < 0 and la_end_byte < 0: continue
+
+			la_start_bytes_set.add(la_start_byte)
+			la_end_bytes_set.add(la_end_byte)
+			long_answer_content = ""
+			for l in line_dict['document_tokens']:
+				if l['start_byte'] >= la_start_byte and l['end_byte'] <= la_end_byte and l['html_token'] is False:
+					 long_answer_content += l['token'] + " "
+			long_answers.append(long_answer_content)	
+
+		lac_starts = collections.defaultdict(list)
+		lac_ends = collections.defaultdict(list)
+		i = 0
+		for lac in random.sample(line_dict['long_answer_candidates'], len(line_dict['long_answer_candidates'])):
+			if lac['start_byte'] in la_start_bytes_set and lac['end_byte'] in la_end_bytes_set:
+				continue
+			start_byte = lac['start_byte']
+			end_byte = lac['end_byte']
+			lac_starts[start_byte].append(i)
+			lac_ends[end_byte].append(i)
+			i += 1
+
+		lac_contents = ["" for x in range(len(lac_starts))]
+		curr_spans = set()
+		for l in line_dict['document_tokens']:
+
+			if l['start_byte'] in lac_starts:
+				for x in lac_starts[l['start_byte']]:
+					curr_spans.add(x)
+
+			if l['end_byte'] in lac_ends:
+				for x in lac_ends[l['end_byte']]:
+					curr_spans.remove(x)
+
+			for span in curr_spans:
+				lac_contents[span] += l['token'] + " "
+		
+		return [question_text, long_answers, lac_contents]
+
+def preprocess_data(full_data, training=True):
 	qa_ds = []
-	c = 0
-	for i, l in enumerate(full_data):
-		if c%1000 == 0: print(c, '/', len(full_data))
+	print(len(full_data))
+	for i, l in tqdm(enumerate(full_data)):
 		json_data = json.loads(l)
-		qa = extract_qa(json_data)
-		qa_ds.append(qa)
-		c += 1
+		qa = extract_qa(json_data, training)
+		if qa: qa_ds.append(qa)
 	return [a for a in qa_ds if len(a[1]) > 0]
 
-#print("\n\nNow preprocessing the training datasets...")
-#qa_dataset = []
-#num_train_files = 0
-#output_file = os.path.join(train_dir, "train_processed.csv")
-#
-#with open(output_file, 'w') as csvfile:
-#	for train_file in sorted(train_data_files):
-#		train_file = os.path.join(train_dir, train_file)
-#		print(num_train_files, ": ", train_file)
-#
-#		with open(train_file) as f:
-#			content = f.readlines()
-#			qa = preprocess_data(content)
-#			qa_dataset += qa
-#		del content
-#		f.close()
-#		num_train_files += 1
-#		print("Number of examples so far: ", len(qa_dataset))
-#
-#		for qa in qa_dataset:
-#			csvfile.write(qa[0] + " <;;;> " + " 1 " + " <;;;> " + qa[1] + "\n")
-#			for cand in qa[2]:
-#				csvfile.write(qa[0] + " <;;;> " + " 0 " + " <;;;> " + cand + "\n")
-
-#del qa_dataset
-#csvfile.close()
-
-print("\n\nNow preprocessing the dev datasets...")
+print("\n\nNow preprocessing the training datasets...")
 qa_dataset = []
-num_dev_files = 0
-output_file = os.path.join(dev_dir, "dev_processed.csv")
+num_train_files = 0
+output_file = os.path.join(train_dir, "train_processed.csv")
 
-with open(output_file, "w") as csvfile:
-	csvfile.write("question" + " <;;;> " + "label" + " <;;;> " + "candidate")
-	for dev_file in sorted(dev_data_files):
-		dev_file = os.path.join(dev_dir, dev_file)
-		print(num_dev_files, ": ", dev_file)
+with open(output_file, 'w') as csvfile:
+	csvfile.write("question" + " <;;;> " + "label" + " <;;;> " + "candidate\n")
+	for train_file in sorted(train_data_files):
+		train_file = os.path.join(train_dir, train_file)
+		print(num_train_files, ": ", train_file)
 
-		with open(dev_file) as f:
+		with open(train_file) as f:
 			content = f.readlines()
 			qa = preprocess_data(content)
 			qa_dataset += qa
 		del content
 		f.close()
-		num_dev_files += 1
+		num_train_files += 1
 		print("Number of examples so far: ", len(qa_dataset))
 
 		for qa in qa_dataset:
@@ -127,4 +170,34 @@ with open(output_file, "w") as csvfile:
 			for cand in qa[2]:
 				csvfile.write(qa[0] + " <;;;> " + " 0 " + " <;;;> " + cand + "\n")
 
+del qa_dataset
+csvfile.close()
 
+print("\n\nNow preprocessing the dev datasets...")
+qa_dataset = []
+num_dev_files = 0
+output_file = os.path.join(dev_dir, "dev_processed.csv")
+
+with open(output_file, "w") as csvfile:
+	csvfile.write("question" + " <;;;> " + "label" + " <;;;> " + "candidate\n")
+	for dev_file in sorted(dev_data_files):
+		dev_file = os.path.join(dev_dir, dev_file)
+		print(num_dev_files, ": ", dev_file)
+
+		with open(dev_file) as f:
+			content = f.readlines()
+			qa = preprocess_data(content, False)
+			qa_dataset += qa
+		del content
+		f.close()
+		num_dev_files += 1
+		print("Number of examples so far: ", len(qa_dataset))
+
+		for qa in qa_dataset:
+			for la in qa[1]:
+				csvfile.write(qa[0] + " <;;;> " + " 1 " + " <;;;> " + la + "\n")
+			for cand in qa[2]:
+				csvfile.write(qa[0] + " <;;;> " + " 0 " + " <;;;> " + cand + "\n")
+
+del qa_dataset
+csvfile.close()
--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import sys, os
 import matplotlib.pyplot as plt
 import copy
@@ -11,7 +12,8 @@ import pandas as pd
 from torch.utils.data import DataLoader, Dataset
 #from torch.cuda.amp import autocast, GradScaler
 from tqdm import tqdm
-from transformers import AutoTokenizer, AutoModel, AdamW, get_linear_schedule_with_warmup
+from transformers import AutoTokenizer, AutoModel, DistilBertModel, AdamW, get_linear_schedule_with_warmup
+from sklearn.metrics import f1_score
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
@@ -55,28 +57,39 @@ class SentencePairClassifier(nn.Module):
 	def __init__(self, bert_model="albert-base-v2", freeze_bert=False):
 		super(SentencePairClassifier, self).__init__()
 
-		#  Instantiating BERT-based model object
-		self.bert_layer = AutoModel.from_pretrained(bert_model)
+		# Instantiating BERT-based model object
+		#self.bert_layer = AutoModel.from_pretrained(bert_model)
 
 		#  Fix the hidden-state size of the encoder outputs (If you want to add other pre-trained models here, search for the encoder output size)
 		if bert_model == "albert-base-v2":	# 12M parameters
+			self.bert_layer = AutoModel.from_pretrained(bert_model)
 			hidden_size = 768
 		elif bert_model == "albert-large-v2":  # 18M parameters
+			self.bert_layer = AutoModel.from_pretrained(bert_model)
 			hidden_size = 1024
 		elif bert_model == "albert-xlarge-v2":	# 60M parameters
+			self.bert_layer = AutoModel.from_pretrained(bert_model)
 			hidden_size = 2048
 		elif bert_model == "albert-xxlarge-v2":  # 235M parameters
+			self.bert_layer = AutoModel.from_pretrained(bert_model)
 			hidden_size = 4096
 		elif bert_model == "bert-base-uncased": # 110M parameters
+			self.bert_layer = AutoModel.from_pretrained(bert_model)
 			hidden_size = 768
-		
+		elif bert_model == "distilbert-base-uncased": #66M parameteres
+			self.bert_layer = DistilBertModel.from_pretrained(bert_model)
+			hidden_size = 768
+		elif bert_model == 'distilbert-base-uncased-distilled-squad': #66M parameters
+			self.bert_layer = DistilBertModel.from_pretrained(bert_model)
+			hidden_size = 768
+
 		# Freeze bert layers and only train the classification layer weights
-		if freeze_bert:
-			for p in self.bert_layer.parameters():
-				p.requires_grad = False
+		#if freeze_bert:
+		#	for p in self.bert_layer.parameters():
+		#		p.requires_grad = False
 
 		# Classification layer
-		self.cls_layer = nn.Linear(hidden_size, 1)
+		self.cls_layer1 = nn.Linear(hidden_size, 1)
 		self.dropout = nn.Dropout(p=0.1)
 
 	#@autocast()  # run in mixed precision
@@ -89,15 +102,24 @@ class SentencePairClassifier(nn.Module):
 		'''
 
 		# Feeding the inputs to the BERT-based model to obtain contextualized representations
-		cont_reps, pooler_output = self.bert_layer(input_ids, attn_masks, token_type_ids) #TODO 
-		#pooler_output = self.bert_layer(input_ids, attn_masks, token_type_ids)  
+		inputs = {"input_ids": input_ids, "attention_mask": attn_masks, "token_type_ids": token_type_ids,}
+		c, pooler_output = self.bert_layer(**inputs)  
+
+		#inputs = {"input_ids": input_ids, "attention_mask": attn_masks,}
+		#output = self.bert_layer(**inputs)  
 
 		# Feeding to the classifier layer the last layer hidden-state of the [CLS] token further processed by a
-		# Linear Layer and a Tanh activation. The Linear layer weights were trained from the sentence order prediction (ALBERT) or next sentence prediction (BERT)
+		# Linear Layer and a Tanh activation. 
+		# The Linear layer weights were trained from the sentence order prediction (ALBERT) 
+		# or next sentence prediction (BERT)
 		# objective during pre-training.
-		logits = self.cls_layer(self.dropout(pooler_output))
-		return logits
+		#list_out = []
+		#for i in range(len(output[0])):	
+		#	list_out.append(output[0][i][0].unsqueeze(0))
+		#pooler_output = torch.cat(list_out, dim=0)
 
+		logits = self.cls_layer1(self.dropout(pooler_output))
+		return logits
 
 def set_seed(seed):
 	""" Set all seeds to make results reproducible """
@@ -113,14 +135,24 @@ def evaluate_loss(net, device, criterion, dataloader):
 	net.eval()
 	mean_loss = 0
 	count = 0
+	out_labels = []
+	out_preds = []
 	with torch.no_grad():
 		for it, (seq, attn_masks, token_type_ids, labels) in enumerate(tqdm(dataloader)):
 			seq, attn_masks, token_type_ids, labels = \
 				seq.to(device), attn_masks.to(device), token_type_ids.to(device), labels.to(device)
 			logits = net(seq, attn_masks, token_type_ids)
+			preds = F.sigmoid(logits)
+			out_labels.append(labels)
+			out_preds.append(preds)
 			mean_loss += criterion(logits.squeeze(-1), labels.float()).item()
 			count += 1
 
+	all_preds = torch.cat(out_preds, dim=0).squeeze(-1)
+	#print(all_preds>0.5)
+	all_labels = torch.cat(out_labels, dim=0)
+	#print(all_labels)
+	print("\nF1-SCORE: ", f1_score(all_preds>0.5, all_labels))
 	return mean_loss / count
 
 
@@ -134,19 +166,15 @@ def train_bert(net, criterion, opti, lr, lr_scheduler, train_loader, val_loader,
 	val_losses = []
 
 	#scaler = GradScaler()
+	model_counter = 0
+	net.train()
 	for ep in range(epochs):
-		net.train()
 		running_loss = 0.0
 		for it, (seq, attn_masks, token_type_ids, labels) in enumerate(tqdm(train_loader)):
 			# Converting to cuda tensors
-			seq, attn_masks, token_type_ids, labels = \
-				seq.to(device), attn_masks.to(device), token_type_ids.to(device), labels.to(device)
+			seq, attn_masks, token_type_ids, labels = seq.to(device), attn_masks.to(device), token_type_ids.to(device), labels.to(device)
 
-			# Enables autocasting for the forward pass (model + loss)
-			#with autocast(): #TODO
-
-				# Obtaining the logits from the model
-
+			opti.zero_grad()
 			logits = net(seq, attn_masks, token_type_ids)
 
 			# Computing loss
@@ -154,22 +182,11 @@ def train_bert(net, criterion, opti, lr, lr_scheduler, train_loader, val_loader,
 			loss = loss / iters_to_accumulate  # Normalize the loss because it is averaged
 
 			# Backpropagating the gradients
-			# Scales loss.	Calls backward() on scaled loss to create scaled gradients.
-			#scaler.scale(loss).backward()
 			loss.backward() #originially the line above is there, not this line
+			opti.step()
 
-			if (it + 1) % iters_to_accumulate == 0: #TODO
-				# Optimization step
-				# scaler.step() first unscales the gradients of the optimizer's assigned params.
-				# If these gradients do not contain infs or NaNs, opti.step() is then called,
-				# otherwise, opti.step() is skipped.
-				#scaler.step(opti)
-				# Updates the scale for next iteration.
-				#scaler.update()
-				# Adjust the learning rate based on the number of iterations.
-				lr_scheduler.step()
-				# Clear gradients
-				opti.zero_grad()
+			#if (it + 1) % iters_to_accumulate == 0: #TODO
+			#lr_scheduler.step()
 
 			running_loss += loss.item()
 
@@ -178,22 +195,22 @@ def train_bert(net, criterion, opti, lr, lr_scheduler, train_loader, val_loader,
 				print("Iteration {}/{} of epoch {} complete. Loss : {} "
 					  .format(it+1, nb_iterations, ep+1, running_loss / print_every))
 
+				# Saving the model
+				path_to_model='/home/ubuntu/mnt/cloudNAS2/SoftKBase/Google_NQ/models/{}_lr_{}_val_loss_{}_ep_{}_{}.pt'.format(bert_model, lr, round(running_loss, 5), ep, model_counter)
+				torch.save(net.state_dict(), path_to_model)
+				print("The model has been saved in {}".format(path_to_model))
+				model_counter += 1
+
+				val_loss = evaluate_loss(net, device, criterion, val_loader)  # Compute validation loss
+				print()
+				print("Epoch {} complete! Validation Loss : {}".format(ep+1, val_loss))
+
 				running_loss = 0.0
 
-		val_loss = evaluate_loss(net, device, criterion, val_loader)  # Compute validation loss
-		print()
-		print("Epoch {} complete! Validation Loss : {}".format(ep+1, val_loss))
-
-		if val_loss < best_loss:
-			print("Best validation loss improved from {} to {}".format(best_loss, val_loss))
-			print()
-			net_copy = copy.deepcopy(net)  # save a copy of the model
-			best_loss = val_loss
-			best_ep = ep + 1
 
 	# Saving the model
-	path_to_model='/home/ubuntu/mnt/cloudNAS2/SoftKBase/Google_NQ/models/{}_lr_{}_val_loss_{}_ep_{}.pt'.format(bert_model, lr, round(best_loss, 5), best_ep)
-	torch.save(net_copy.state_dict(), path_to_model)
+	path_to_model='/home/ubuntu/mnt/cloudNAS2/SoftKBase/Google_NQ/models/{}_lr_{}_val_loss_{}_ep_{}_final.pt'.format(bert_model, lr, round(best_loss, 5), best_ep)
+	torch.save(net.state_dict(), path_to_model)
 	print("The model has been saved in {}".format(path_to_model))
 
 	del loss
@@ -202,55 +219,60 @@ def train_bert(net, criterion, opti, lr, lr_scheduler, train_loader, val_loader,
 
 # Reading train and dev data
 data_dir = sys.argv[1]
-train_path = open(os.path.join(data_dir,'train/train_processed_0.csv'))
+train_path = open(os.path.join(data_dir,'train/train_processed.csv'))
 dev_path = open(os.path.join(data_dir,'dev/dev_processed.csv'))
 
-delimiter = " <;;;> " 
-df_train = pd.read_csv(train_path, delimiter=delimiter)
+#train_path = open(os.path.join(data_dir,'dev/dev_processed_0.csv'))
+#dev_path = open(os.path.join(data_dir,'dev/dev_processed_0.csv'))
+
+delimiter = " <;;;> "
 df_val = pd.read_csv(dev_path, delimiter=delimiter)
+chunksize = 6708167
+#chunksize = 100
+for df_train in pd.read_csv(train_path, delimiter=delimiter, chunksize=chunksize):
+	# Defining model and parameters #TODO
+	bert_model = 'bert-base-uncased' 
 
-print(df_train.head())
-print(df_train.columns)
-print(df_train['label'].value_counts())
-print(len(df_train.index))
-print(df_train.shape)
-print(df_train.dtypes)
+	#bert_model = 'albert-base-v2'
+	#bert_model = 'albert-large-v2' 
+	#bert_model = 'albert-large-v2'
+	#bert_model = 'albert-xlarge-v2'
+	#bert_model = 'albert-xxlarge-v2'
 
-# Defining model and parameters #TODO
-bert_model = "bert-base-uncased"  # 'albert-base-v2', 'albert-large-v2', 'albert-xlarge-v2', 'albert-xxlarge-v2', 'bert-base-uncased', ...
-freeze_bert = False  # if True, freeze the encoder weights and only update the classification layer weights
-maxlen = 192  # maximum length of the tokenized input sentence pair : if greater than "maxlen", the input is truncated and else if smaller, the input is padded
-bs = 16  # batch size
-iters_to_accumulate = 1  # the gradient accumulation adds gradients over an effective batch of size : bs * iters_to_accumulate. If set to "1", you get the usual batch size
-lr = 5e-5  # learning rate
-epochs = 4  # number of training epochs
+	#bert_model = 'distilbert-base-uncased' 
+	#bert_model = 'distilbert-base-uncased-distilled-squad'
 
-#  Set all seeds to make reproducible results
-set_seed(1)
+	freeze_bert = False	# if True, freeze the encoder weights and only update the classification layer weights
+	maxlen = 512  # maximum length of the tokenized input sentence pair : if greater than "maxlen", the input is truncated and else if smaller, the input is padded
+	bs = 4  # batch size
+	iters_to_accumulate = 1  # the gradient accumulation adds gradients over an effective batch of size : bs * iters_to_accumulate. If set to "1", you get the usual batch size
+	lr = 1e-6  # learning rate
+	epochs = 5	# number of training epochs
 
-# Creating instances of training and validation set
-print("Reading training data...")
-train_set = CustomDataset(df_train, maxlen, bert_model)
-print("Reading validation data...")
-val_set = CustomDataset(df_val, maxlen, bert_model)
+	#  Set all seeds to make reproducible results
+	set_seed(1)
 
-# Creating instances of training and validation dataloaders
-train_loader = DataLoader(train_set, batch_size=bs, num_workers=1)
-val_loader = DataLoader(val_set, batch_size=bs, num_workers=5)
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-net = SentencePairClassifier(bert_model, freeze_bert=freeze_bert)
+	# Creating instances of training and validation set
+	print("Reading training data...")
+	train_set = CustomDataset(df_train, maxlen, bert_model)
+	print("Reading validation data...")
+	val_set = CustomDataset(df_val, maxlen, bert_model)
 
-if torch.cuda.device_count() > 1:  # if multiple GPUs
-    print("Let's use", torch.cuda.device_count(), "GPUs!")
-    net = nn.DataParallel(net)
+	# Creating instances of training and validation dataloaders
+	train_loader = DataLoader(train_set, batch_size=bs, num_workers=1)
+	val_loader = DataLoader(val_set, batch_size=bs, num_workers=1)
+	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+	net = SentencePairClassifier(bert_model, freeze_bert=freeze_bert)
 
-net.to(device)
+	net.to(device)
 
-criterion = nn.BCEWithLogitsLoss()
-opti = AdamW(net.parameters(), lr=lr, weight_decay=1e-2)
-num_warmup_steps = 0 # The number of steps for the warmup phase.
-num_training_steps = epochs * len(train_loader)  # The total number of training steps
-t_total = (len(train_loader) // iters_to_accumulate) * epochs  # Necessary to take into account Gradient accumulation
-lr_scheduler = get_linear_schedule_with_warmup(optimizer=opti, num_warmup_steps=num_warmup_steps, num_training_steps=t_total)
+	criterion = nn.BCEWithLogitsLoss()
+	opti = AdamW(net.parameters(), lr=lr, weight_decay=1e-2)
+	num_warmup_steps = 0 # The number of steps for the warmup phase.
+	num_training_steps = epochs * len(train_loader)  # The total number of training steps
+	t_total = (len(train_loader) // iters_to_accumulate) * epochs  # Necessary to take into account Gradient accumulation
+	lr_scheduler = get_linear_schedule_with_warmup(optimizer=opti, num_warmup_steps=num_warmup_steps, num_training_steps=t_total)
 
-train_bert(net, criterion, opti, lr, lr_scheduler, train_loader, val_loader, epochs, iters_to_accumulate)
+	train_bert(net, criterion, opti, lr, lr_scheduler, train_loader, val_loader, epochs, iters_to_accumulate)
+
+
