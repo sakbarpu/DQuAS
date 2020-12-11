359eae5e53b5e748be34bad0311708667d46c4cc
Ubuntu <ubuntu@CloudCompute>
2020-12-10 23:09:12 -0500

    cleaned up download dataset script

--- download_dataset.py
+++ download_dataset.py
@@ -1,23 +1,39 @@
-import os
+import sys, os
 
+data_dir = sys.argv[1]
+
+# Where to get the data from the internel
 base = "https://storage.googleapis.com/natural_questions/v1.0"
-train_urls =  ["%s/train/nq-train-%02d.jsonl.gz" % (base, i) for i in range(50)]
-dev_urls = ["%s/dev/nq-dev-%02d.jsonl.gz" % (base, i) for i in range(5)]
+train_urls =  ["%s/train/nq-train-%02d.jsonl.gz" % (base, i) for i in range(50)] #there are 50 train files
+dev_urls = ["%s/dev/nq-dev-%02d.jsonl.gz" % (base, i) for i in range(5)] #and 5 dev files
 
+print("Following are the urls for train dataset")
 print(train_urls)
+print("Following are the urls for dev dataset")
 print(dev_urls)
 
-#for t_url in train_urls:
-#	os.system("wget " + t_url + " -P " + "train")
+# Downloading train file from the urls
+for t_url in train_urls:
+	try:
+		os.system("wget " + t_url + " -P " + os.path.join(data_dir,"train"))
+	except OSError as e:
+		raise OSError("See if wget is installed")
 
-for d_url in dev_urls:
-	os.system("wget " + d_url + " -P " + "dev")
+# Downloading dev files from the urls
+for d_url in dev_urls
+	try:
+		os.system("wget " + d_url + " -P " + os.path.join(data_dir, "dev"))
+	except OSError as e:
+		raise OSError("See if wget is installed")
 
-#make sure gzip is installed
+# Go to train dir and do unzip
+try:
+	os.system("cd " + os.path.join(data_dir,"train") + " ; " + "for f in nq-train-*l.gz; do echo $f; gzip -d $f; done;")
+except OSError as e:
+	raise OSError("See if gunzip is installed")
 
-#go to train dir and do
-#for f in nq-train-*l.gz; do echo $f; gzip -d $f; done;
-
-#go to dev dir and do
-#for f in nq-dev-*l.gz; do echo $f; gzip -d $f; done;	
-
+# Go to dev dir and do unzip
+try:
+	os.system("cd " + os.path.join(data_dir,"dev") + " ; " + "for f in nq-dev-*l.gz; do echo $f; gzip -d $f; done;")
+except OSError as e:
+	raise OSError("See if gunzip is installed")
